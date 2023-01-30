514cd0d31bdc73153bdb5cc3e7c52f0633eb31af
davidgfb <davidgracia99@gmail.com>
2023-01-30 16:47:23 +0100

    Update py.py

--- py.py
+++ py.py
@@ -10,7 +10,7 @@ for beta in range(-fov, fov):
     for alfa in range(-fov, fov): #(angZ = -fov; angZ < fov; angZ++) { #partimos desde esq sup izda
         if alfa < 0: alfa += 180; #4º --> 1er cuad
 
-        #print(angX, angZ)
+        #print(alfa, beta)
         
         alfa_Rads = radians(float(alfa)) #, t = map(posCam); //partimos desde p0 = posCam getDistMinAlObj
         dir_Rayo = normalize((cos(alfa_Rads), sin(alfa_Rads), 0)); #vec3 p1 = rayCast(normalize(0, cos(ang), 0)); //avanzamos la distancia t en la direccion del rayo           
