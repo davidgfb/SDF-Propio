8e5c2ec6cb8857f6f04ce61eb180c13fef62b94a
davidgfb <davidgracia99@gmail.com>
2023-01-30 16:50:12 +0100

    Update py.py

--- py.py
+++ py.py
@@ -6,11 +6,13 @@ fov = 45;
 def normalize(dire):     
     return dire / norm(dire)
 
-for beta in range(-fov, fov): 
-    for alfa in range(-fov, fov): #(angZ = -fov; angZ < fov; angZ++) { #partimos desde esq sup izda
-        if alfa < 0: alfa += 180; #4º --> 1er cuad
+r = range(-fov, fov)
 
-        #print(alfa, beta)
+for beta in r: 
+    for alfa in r: #(angZ = -fov; angZ < fov; angZ++) { #partimos desde esq sup izda
+        if alfa < 0: alfa += 180; #4º --> 1er cuad
+            
+        print(beta, alfa)
         
         alfa_Rads = radians(float(alfa)) #, t = map(posCam); //partimos desde p0 = posCam getDistMinAlObj
         dir_Rayo = normalize((cos(alfa_Rads), sin(alfa_Rads), 0)); #vec3 p1 = rayCast(normalize(0, cos(ang), 0)); //avanzamos la distancia t en la direccion del rayo           
