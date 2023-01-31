53cc29e507033d9ca684302a07abb65394040f07
davidgfb <davidgracia99@gmail.com>
2023-01-31 22:21:01 +0100

    Update ultimo.glsl

--- ultimo.glsl
+++ ultimo.glsl
@@ -47,11 +47,12 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord ) {
          
     while (!esCero && t <= tMin) { //'=' para el 1er paso
         ro += rd * t;
-        t = map(ro); 
-        esCero = getEsCero(t);
+        float t1 = map(ro); 
+        t = t1;
+        esCero = getEsCero(t1);
         
-        if (t < tMin) {
-            tMin = t;
+        if (t1 < tMin) {
+            tMin = t1;
         }
     }
     
