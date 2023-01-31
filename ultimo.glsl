6fa8a4db63a49f060960a50a51d5dc49554122bb
davidgfb <davidgracia99@gmail.com>
2023-01-31 16:58:31 +0100

    Update ultimo.glsl

--- ultimo.glsl
+++ ultimo.glsl
@@ -9,11 +9,17 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord ) {
     iResolution = resolucion pantalla
     ej: 1er pixel: (0.5, 0.5) / (1920, 1080) = (0, 0)
     n-esimo pixel: (1919'5, 1079'5) / (1920, 1080) =
-    = (1, 1)    
+    = (1, 1)  
+    
+    rd normaliza uv entre [-1, 1]
+    ej: (0, 0): 2 * (0, 0) - (1, 1) = (0, 0) - (1, 1) =
+    = (-1, -1) esq inf izda
+    (1, 1): 2 * (1, 1) - (1, 1) = (2, 2) - (1, 1) =
+    = (1, 1) esq sup dcha
     */
     vec2 uv = fragCoord / iResolution.xy; 
     vec3 ro = vec3(0, 0, 1),
-        rd = normalize(vec3((2.0 * uv - 1.0) * 
+        rd = normalize(vec3((2.0 * uv - vec2(1)) * 
         vec2(iResolution.x / iResolution.y, 1), -1.0)),
         color = vec3(0);
     float tMin = map(ro),
