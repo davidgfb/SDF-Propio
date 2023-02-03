dc47c16213f6e873bc5e41523a0d4d186e0efe0a
davidgfb <davidgracia99@gmail.com>
2023-02-03 11:51:31 +0100

    Update Main.glsl

--- Main.glsl
+++ Main.glsl
@@ -1,6 +1,12 @@
 const float h = 1e-3, h1 = h / 10.0; //h para gradiente
 vec3 y = vec3(0, 1, 0);
 
+struct vec5 {
+    vec3 c;
+    float a;
+    bool con;   
+};
+
 float map(vec3 p) {
     float r = 0.5;
     
@@ -15,22 +21,26 @@ bool esMayorQ_Cero(float t) {
     return t > h1;
 }
 
-vec3 getNormal(vec3 p) { //gradiente normaliza entre [0, 1]. Ej: (-1 + 1) / 2 = 0, (1 + 1) / 2 = 1
+vec3 getNormal(vec3 p) { 
+    /*gradiente normaliza entre [0, 1]. Ej: (-1 + 1) / 2 = 0, (1 + 1) / 2 = 1
+    */
     return (normalize(map(p) - vec3(map(vec3(-h, 0, 0) + p), 
         map(-h * y + p), map(vec3(0, 0, -h) + p))) + 1.0) / 2.0;
 }
 
-/*vec4 rayMarch(bool cond, int nPasos, vec3 ro, vec3 rd, float t) {
-    for (int i = 0; cond && i < nPasos; i++) { 
+vec5 rayMarch(bool cond, int nPasos, vec3 ro, vec3 rd, float t, bool cond1) {
+    for (int i = 0; cond && i < nPasos; i++) { //bEsCero = false --> !bEsCero = true 
         ro += rd * t;        
-        t = map(ro);                   
-        //esCero = getEsCero(t);        
+        t = map(ro); 
+        cond = !esCero(t);
+        
+        if (cond1) cond = !esMayorQ_Cero(t); 
     }
     
-    return vec4(ro, t);
+    return vec5(ro, t, cond); //vec5(vec4(ro, t), cond);
 }
 
-vec4 rayMarch(bool cond, int nPasos, vec3 ro, vec3 rd, float t) {
+/*vec4 rayMarch(bool cond, int nPasos, vec3 ro, vec3 rd, float t) {
     return rayMarch(cond, nPasos, ro, rd, t, false);
 }*/
 
@@ -42,39 +52,41 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord ) {
         fragCoord - vec2(1)), 1)), //z = 1      
         color = vec3(0); 
     //vec3 posLuz = vec3(1); //pto luz 
-    float t = map(ro); 
+    float t = map(ro);
     bool cond = esCero(t);
-        
+    
     rd.x *= iResolution.x / iResolution.y;                 
     rd = rd.xzy; //z --> y = 1, y --> z, x cte         
     
-    for (int i = 0; !cond && i < nPasos_Luz; i++) { 
+    /*for (int i = 0; !bEsCero && i < nPasos_Luz; i++) { 
         ro += rd * t;        
         t = map(ro);         
-        cond = esCero(t);
-    }
+        bEsCero = esCero(t);
+    }*/
     
-    /*vec4 vRayMarch = rayMarch(!getEsCero(t), nPasos_Luz, ro, rd, t);
-    ro = vRayMarch.xyz;
-    t = vRayMarch.a;*/
+    vec5 vRayMarch = rayMarch(!cond, nPasos_Luz, ro, rd, t, false);
+    ro = vRayMarch.c;
+    t = vRayMarch.a;
+    cond = vRayMarch.con;
     
     color = getNormal(ro); //no hay contacto (i = 1000)  
         
     if (cond) { //sombra directa 
         cond = esMayorQ_Cero(t);
-        rd = vec3(1); //luz direccional //vec3(0, 1, 0); //normalize(posLuz - ro);
+       
+        rd = vec3(1); //luz direccional //vec3(0, 1, 0); //normalize(posLuz - ro);                
+        vRayMarch = rayMarch(!cond, nPasos_Sombra, ro, rd, t, true); //t > h1, nPasos_Sombra, ro, rd, t);
+        ro = vRayMarch.c;
+        t = vRayMarch.a;
         
-        /*vec4 vRayMarch = rayMarch(t > h1, nPasos_Sombra, ro, rd, t);
-        ro = vRayMarch.xyz;
-        t = vRayMarch.a;*/
-        
-        for (int i = 0; cond && i < nPasos_Sombra; i++) { 
+        /*for (int i = 0; cond && i < nPasos_Sombra; i++) { 
             ro += rd * t;        
             t = map(ro); 
-            cond = esMayorQ_Cero(t);
+            //cond = esMayorQ_Cero(t);
         }
-   
-        if (!cond) color -= vec3(0.1);         
+          
+        if (!cond) color -= vec3(0.1);*/
+        if (!vRayMarch.con) color -= vec3(0.1);         
     }
          
     fragColor = vec4(color, 1);    
