f41e9a0b81abeedfcf5b28f83c6319359f2bbe1f
davidgfb <davidgracia99@gmail.com>
2023-04-09 13:14:48 +0200

    Update Main.glsl

--- Main.glsl
+++ Main.glsl
@@ -16,15 +16,15 @@ struct Rayo {
 }*/
 
 float supcie(vec2 p) {
-    return 0.0; //sin(p.x) * sin(p.y);
+    return sin(p.x) * sin(p.y); //0.0;
 }
 
-vec3 posEsf = vec3(-1, 1, 0); //z es calculado, calcula BIEN!!! pueden ser las normales!!!
+vec3 posEsf = vec3(-vec2(-1, 1), 0); //z es calculado, calcula BIEN!!! pueden ser las normales!!!
 float mapLuz(vec3 p) {
     posEsf = vec3(posEsf.xy, supcie(posEsf.xy));
 
     float r = 0.5,
-        esf = length(p - posEsf) - r; //p - INVERTIDO? x q NO funciona bien?
+        esf = length(p + posEsf) - r; //p - INVERTIDO? x q NO funciona bien?
     
     posEsf.x = sin(iTime); //posEsf += y.xy * iTime; //+= NO sirve!!!
     
