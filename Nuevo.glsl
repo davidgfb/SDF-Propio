f9378077095167bfcb2395ef59a8948fa107bfa1
davidgfb <davidgracia99@gmail.com>
2023-10-29 15:51:39 +0100

    Update Nuevo.glsl

--- Nuevo.glsl
+++ Nuevo.glsl
@@ -10,9 +10,9 @@ todas las direcciones deben estar normalizadas
 Todas estas variables se recargan x cada pixel.
 Ojo a que sean constantes
 */
-vec3 z = vec3(0, 0, 1), p_Min_Esfera = vec3(1e3); 
-//H_Min_Esfera = p_Min_Esfera.z
 float h_Plano = -200.0, r_Esfera = 100.0;
+vec3 z = vec3(0, 0, 1), p_Min_Esfera = vec3(0,0,-100); //vec3(1e3); 
+//H_Min_Esfera = p_Min_Esfera.z
 int f_Actual = 0;
 
 float d_Esfera(vec3 p, float r) { 
@@ -24,36 +24,38 @@ float d_Plano( vec3 p, vec3 n, float h ) {
     return dot(p,-n) - h;
 }
 
-void act_Frametime() {
-    /*
-    estas variables son ctes durante frameTime
-    actualiza solo UNA vez x frametime
-    */
-    float g = 9.8, v_Term = 55.0, v = 0.0; //, v = g/2.0*iTime; //v(11) = v_Term
-        
-    if (v < v_Term) v = g/2.0*iTime;
-    
-    else if (v > v_Term) v = v_Term; //UNA vez
-    
-    h_Plano += v*iTime;
-        
-    /*
-    movto SIEMPRE en f(t).
-    NO valen operadores de asignacion compuesta que no esten en f de 
-    iTime! x la volatilidad/efimeridad de los datos solo actualiza 
-    para el frametime y no conserva valores de variables/datos
-    */
-    f_Actual = iFrame;
-}
-
 /*
 actualizo posicion cada frame en f de tiempo. 
 iTimeDelta (f) = Tiempo/frame, 16ms/frame
 iFrameRate(t)
 */
 float d_Supcie(vec3 p) {
-    if (f_Actual != iFrame) act_Frametime();
-        
+    if (f_Actual != iFrame) { 
+        /*
+        estas variables son ctes durante frameTime
+        actualiza solo UNA vez x frametime
+        */
+        float g = 9.8, v_Term = 55.0, v = 0.0; //v(11) = v_Term
+
+        //v != v_Term
+        //float - vec3!!
+        if (/*p_Min_Esfera.z*/-r_Esfera-h_Plano > 0.0) { 
+            if (v < v_Term) v = g/2.0*iTime; 
+            else if (v > v_Term) v = v_Term; 
+
+        } else if (v > 0.0) v = 0.0; //NO funca
+
+        h_Plano += v*iTime;
+
+        /*
+        movto SIEMPRE en f(t).
+        NO valen operadores de asignacion compuesta que no esten en f de 
+        iTime! x la volatilidad/efimeridad de los datos solo actualiza 
+        para el frametime y no conserva valores de variables/datos
+        */
+        f_Actual = iFrame;
+    }
+    
     return min(d_Esfera(p, r_Esfera),d_Plano(p, z, h_Plano)); 
 }
 
@@ -81,12 +83,19 @@ void mainImage( out vec4 fragColor, vec2 fragCoord ) {
     for (p; length(p-o) < d_Max;) { //while    
         p += v*f_D_Supcie;
               
-        if (f_D_Supcie < 1e-5) { //f de movimiento SIEMPRE antes de col!
-            col = normalize(col-p); //col direccion, vec3(1);
-            
-            //es_Esfera
+        if (f_D_Supcie < 1e-5) { //f de movimiento SIEMPRE antes de col!         
+            /*
+            es_Esfera
+            se puede calcular marchando rayo desde infierno hasta esfera
+            o desde dentro hacia fuera cuando rayo tenga valores 
+            ascendentes
             if (f_D_Supcie == d_Esfera(p, r_Esfera) && 
                 p_Min_Esfera.z > p.z) p_Min_Esfera = p;
+            esto NO se puede hacer x falta de PERSISTENCIA
+            tmb se podria hacer x GRADIENTE
+            */
+                                            
+            col = normalize(col-p); //col direccion, vec3(1);
         
         } else f_D_Supcie = d_Supcie(p);
     }
