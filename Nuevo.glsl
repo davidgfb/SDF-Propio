40d2b389bc636c7faff1bd4da53cd9cc4a3fc9a8
davidgfb <davidgracia99@gmail.com>
2023-10-29 12:08:21 +0100

    Update Nuevo.glsl

--- Nuevo.glsl
+++ Nuevo.glsl
@@ -6,21 +6,53 @@ fragColor is used as output channel. It is not, for now,
     mandatory but recommended to leave the alpha channel to 1.0.
 vec3 iResolution image/buffer The viewport resolution 
 float iTime	image/sound/buffer Current time in seconds
+todas las direcciones deben estar normalizadas
 */
+vec3 z = vec3(0, 0, 1), p_Min_Esfera = vec3(1e3); 
+//H_Min_Esfera = p_Min_Esfera.z
+float h_Plano = -200.0, r_Esfera = 100.0;
+int f_Actual = 0;
+
 float d_Esfera(vec3 p, float r) { 
     return length(p) - r;
 }
 
 float d_Plano( vec3 p, vec3 n, float h ) {
-  // n must be normalized
-  return dot(p,n) + h;
+    //n direccion
+    return dot(p,-n) - h;
 }
 
-vec3 z = vec3(0, 0, 1);
+/*
+actualizo posicion cada frame en f de tiempo. 
+iTimeDelta (f) = Tiempo/frame, 16ms/frame
+iFrameRate(t)
+*/
+float d_Supcie(vec3 p) {
+    if (f_Actual != iFrame) {
+        h_Plano *= sin(iTime); //NO valen operadores de 
+        //asignacion compuesta que no esten en f de iTime!
+                
+        f_Actual = iFrame;
+    }
+    
+    /*
+    if (int(iTime) != t_Actual) { //iFrame
+        h_Plano += 10.0; //NO funciona
+        
+        t_Actual = int(iTime);
+    }
+    */ 
+    
+    //h_Plano *= sin(iTime);
 
-float d_Supcie(vec3 p) { //d_Esfera->map
-    return min(d_Esfera(p, 100.0),d_Plano(p, -z, 0.0)); 
+    return min(d_Esfera(p, r_Esfera),d_Plano(p, z, h_Plano)); 
 }
+
+/*
+float get_H() {
+    return 0.0;
+}
+*/
 
 void mainImage( out vec4 fragColor, vec2 fragCoord ) {
     /*
@@ -32,24 +64,26 @@ void mainImage( out vec4 fragColor, vec2 fragCoord ) {
     
     float p_Y = 0.0;
     vec3 col = vec3(0), x = z.zxx, y = z.xzx,  
-        o = vec3(fragCoord.x, -p_Y, -fragCoord.y) - 500.0*x + 300.0*z,
-        p = o, v = y; //p.y tiene q ser opuesto
+        o = vec3(fragCoord.x, -p_Y, -fragCoord.y) - 500.0*x + 400.0*z,
+        p = o, v = y; //v direccion, p.y tiene q ser opuesto
            
-    float f_D_Esfera = d_Supcie(p), d_Max = 1.0;
+    float f_D_Supcie = d_Supcie(p), d_Max = 1e0;
     
-    for (d_Max; length(p-o) < d_Max;) { //while
-        p += v*f_D_Esfera;
+    for (p; length(p-o) < d_Max;) { //while    
+        p += v*f_D_Supcie;
               
-        if (f_D_Esfera < 1e-5) col = normalize(col-p); //vec3(1);
+        if (f_D_Supcie < 1e-5) { //f de movimiento SIEMPRE antes de col!
+            col = normalize(col-p); //col direccion, vec3(1);
+            
+            //es_Esfera
+            if (f_D_Supcie == d_Esfera(p, r_Esfera) && 
+                p_Min_Esfera.z > p.z) p_Min_Esfera = p;
         
-        else f_D_Esfera = d_Supcie(p);
+        } else f_D_Supcie = d_Supcie(p);
     }
-        
-    //analogia p = vt en r3
-
-    /* Normalized pixel coordinates (from 0 to 1)
-    vec2 uv = fragCoord/iResolution.xy;
-
+             
+    /*
+    analogia p = vt en r3
     // Time varying pixel color
     vec3 col = 0.5 + 0.5*cos(iTime+uv.xyx+vec3(0,2,4));
     */
